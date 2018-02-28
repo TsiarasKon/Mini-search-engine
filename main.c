a4717d6e1a13fffef24b07feb3edcee1ea0de5e5
Konstantinos Tsiaras <tsiaraskon@gmail.com>
2018-02-28 15:19:10 +0200

    Interface backbone

--- main.c
+++ main.c
@@ -19,7 +19,7 @@ int main(int argc, char *argv[]) {
         fprintf(stderr, "K must be greater than 0.\n");
         return 1;
     }
-    int lines_count = 0;
+    int doc_count = 0;
     printf("%s\n", docfile);
     FILE *fp = fopen(docfile, "r");
     if (fp == NULL) {
@@ -35,15 +35,36 @@ int main(int argc, char *argv[]) {
             fprintf(stderr, "Error in %s - Docs not in order.\n", docfile);
             return 3;
         }
-        printf("%s\n", buffer);
-        lines_count++;
+        printf("%d\n", strlen(buffer));
+        doc_count++;
+        // save docs and create indices
     }
-//    while ((ch = getc(fp)) != EOF) {
-//        if (ch == '\n') {
-//            lines_count++;
-//        }
-//    }
-    printf("Lines: %d\n", lines_count);
+
+    char *command;
+    char *cmds[4];
+    cmds[0] = "/search";
+    cmds[1] = "/df";
+    cmds[2] = "/tf";
+    cmds[3] = "/exit";
+    while (1) {
+        fgets(buffer, bufSize, stdin);
+        strtok(buffer, "\n");     // remove trailing newline character
+        command = strtok(buffer, " ");
+        if (!strcmp(command, cmds[0])) {              // search
+
+        } else if (!strcmp(command, cmds[1])) {       // df
+
+        } else if (!strcmp(command, cmds[2])) {       // tf
+
+        } else if (!strcmp(command, cmds[3])) {       // exit
+            break;
+        } else {
+            printf("Unkown command '%s' - Type a valid command:\n", command);
+            /// Implement /help
+        }
+    }
+
+    free(buffer);
     return 0;
 }
 
